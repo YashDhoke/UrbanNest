3bea0c103cca0a4eea5ea4d570f9c87e26a33cad
YashDhoke <yashdhoke1@gmail.com>
2023-12-27 15:44:43 +0530

    add pre-installation

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>Welcome to UrbanNest – Your Go-To Spot for Sneaker Vibes!</h1>
+      <p className='mb-4 text-slate-700'>UrbanNest is not just a shoe store; it's a haven for fashion enthusiasts and trendsetters. Our mission is to provide an unparalleled shopping experience, offering a curated selection of footwear that resonates with your unique style.</p>
+      <p className='mb-4 text-slate-700'>
+      As a leading online shoe store, UrbanNest is dedicated to helping you step out in confidence, whether you're looking to add a touch of flair to your everyday look or make a statement at a special occasion. Our extensive collection showcases the latest trends, ensuring that you find the perfect pair to express your individuality.
+      </p>
+      <p className='mb-4 text-slate-700'>Explore our carefully curated selection, where comfort meets style, and discover footwear that complements your personality. From casual sneakers to elegant heels, UrbanNest is here to cater to your every step. We take pride in offering not just shoes, but a statement of your unique fashion identity.</p>
+    </div>
   )
 }
